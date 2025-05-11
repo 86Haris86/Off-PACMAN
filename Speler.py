41b96d66f5ed7578e02501135b070ba37a0955b0
86Haris86 <haris.imassodane@vub.be>
2025-05-11 16:24:58 +0200

    Add files via upload

--- Speler.py
+++ Speler.py
@@ -3,46 +3,55 @@ import pygame
 import time
 #import update
 
-from Setting import maze_checker , tijd_van_bonus , zone1 , zone2 ,toegelaten_posities_speler , muurgrootte , refactor
-from Setting import gebruikte_maze, walls, muurgrootte
+from Hulpfuncties import MazeChecker
+#from Main_loop import gebruikte_maze
+global walls
+
 
 #----------------------------------------------------------------------------------------------------------------------#
-
+refactor = 1
+muurgrootte = 30
 # speler
 snelheid_speler = 5
 radius_speler = 14
 levens = 3
+tijd_snelheidboost = 7000
+snelheid_snelheidboost = 1.5
 
 #----------------------------------------------------------------------------------------------------------------------#
 
 # Klasse die de speler representeert en beweging/leven/overdracht regelt
 class Speler:
-    def __init__(self, x, y, scherm, kleur , straal, snelheid , toegelaten_posities , afbeelding = None):
-        self.x, self.y = x, y
+    def __init__(self, x, y, scherm, kleur , straal, snelheid , toegelaten_posities , gebruikte_maze , afbeelding = None):
+        self.x, self.y = (round(x/5)*5) , (round(y/5)*5)
         self.scherm = scherm
         self.kleur = kleur
         self.straal = straal
         self.snelheid = snelheid
         self.toegelaten_posities = toegelaten_posities
+        self.gebruikte_maze = gebruikte_maze
         self.afbeelding = afbeelding
 
-        self.startpositie = (x, y)
+        self.snelheid_standaard = snelheid  # Bewaar de normale snelheid
+        self.startpositie = ((round(x/5)*5) , (round(y/5)*5))
         self.levens = 3
         self.punten = 0
+        self.eetcombo = 0
+        self.gegeten_spoken_teller = 0
+
         self.timer = None
         self.timer_duur = None
-        self.snelheid_standaard = snelheid  # Bewaar de normale snelheid
         self.snelheid_timer = None  # Start op None, wordt gevuld bij boost
         self.snelheid_duur = 5000
-        self.break_walls = False  # power-up: muren kapot
-
+        self.snelheid_duur = 5000
+        #self.break_walls = False  # power-up: muren kapot
 
         self.richting = None  # Toegevoegd: huidige richting ("UP", "DOWN", etc.)
         self.richting2 = "rechts"  # standaard richting voor pacman
         self.animatie_index = 0
         self.animatie_teller = 0
-        self.eetcombo = 0
 
+#------------------------------------------------Speler_tekenen--------------------------------------------------------#
     def draw(self):
         if self.afbeelding:
             img = self.afbeelding[self.animatie_index]
@@ -70,14 +79,16 @@ class Speler:
         if self.animatie_teller % 5 == 0:
             self.animatie_index = (self.animatie_index + 1) % len(self.afbeelding)
 
+#----------------------------------------------Speler_bewegen----------------------------------------------------------#
+
     def Controleren(self, posities):
+        maze_checker = MazeChecker(self.gebruikte_maze, muurgrootte)
         for coordinaat_x, coordinaat_y in posities:
             if not maze_checker.is_valid(coordinaat_x, coordinaat_y, self.toegelaten_posities):
                 return False
         return True
 
-    def move(self,  snelheid_x, snelheid_y):
-
+    def move(self, snelheid_x, snelheid_y):
         # Richting bepalen (voor animatie)
         if snelheid_x > 0:
             self.richting2 = "rechts"
@@ -88,40 +99,36 @@ class Speler:
         elif snelheid_y > 0:
             self.richting2 = "omlaag"
 
-        nieuw_x, nieuw_y = self.x + snelheid_x ,  self.y + snelheid_y
+        import math
 
-        # Controleer of alle randen van de cirkel in toegelaten ruimte zitten
-        te_controleren_posities = [
-            (nieuw_x + self.straal, nieuw_y + self.straal),
-            (nieuw_x + self.straal, nieuw_y - self.straal),
-            (nieuw_x - self.straal, nieuw_y + self.straal),
-            (nieuw_x - self.straal, nieuw_y - self.straal)
-        ]
+        stappen = math.ceil(max(abs(snelheid_x), abs(snelheid_y)))
+        stap_x = snelheid_x / stappen if stappen != 0 else 0
+        stap_y = snelheid_y / stappen if stappen != 0 else 0
 
-        # Power-up “muren kapot”: breek tile==11 als actief
-        if self.break_walls:
-            for coord_x, coord_y in te_controleren_posities:
-                tx = int(coord_x // muurgrootte)
-                ty = int(coord_y // muurgrootte)
-                if gebruikte_maze[ty][tx] ==  11:
-                    # verwijder het corresponderende muur-object
-                    for wall in walls:
-                        if wall.x == tx * muurgrootte and wall.y == ty * muurgrootte:
-                            walls.remove(wall)
-                            break
-         # markeer tile als lege ruimte
-                    gebruikte_maze[ty][tx] = 0
-        geldig = True
-        for coordinaat_x, coordinaat_y in te_controleren_posities:
-            if not maze_checker.is_valid(coordinaat_x, coordinaat_y, toegelaten_posities_speler):
-                geldig = False
-                break
-        if geldig:
-            self.x = nieuw_x
-            self.y = nieuw_y
-            self.update_animatie()
+        for _ in range(stappen):
+            tussen_x = self.x + stap_x
+            tussen_y = self.y + stap_y
 
-    def patrol(self):
+
+            te_controleren_posities = [
+                (tussen_x + self.straal, tussen_y + self.straal),
+                (tussen_x + self.straal, tussen_y - self.straal),
+                (tussen_x - self.straal, tussen_y + self.straal),
+                (tussen_x - self.straal, tussen_y - self.straal)
+            ]
+
+            if self.Controleren(te_controleren_posities):
+                self.x = tussen_x
+                self.y = tussen_y
+            else:
+                break  # Stoppen bij botsing
+
+        self.update_animatie()
+
+
+    def patrol(self, zone1, overdracht1, zone2, overdracht2):
+        self.update_snelheid()
+        self.overdracht(zone1, overdracht1, zone2, overdracht2)
         keys = pygame.key.get_pressed()
         nieuw_x, nieuw_y = self.x + self.snelheid, self.y + self.snelheid
         te_controleren_posities = [
@@ -169,20 +176,62 @@ class Speler:
     def reset(self):
         self.x, self.y = self.startpositie
 
-    def overdracht(self):
-        if zone1 == None and zone2 == None:
-            return
-        else :
-            if (self.x, self.y) == zone1:
-                self.x, self.y = zone2
-            elif (self.x, self.y) == zone2:
-                self.x, self.y = zone1
+    def in_zone(self, zone):
+        zone_x, zone_y, zone_breedte, zone_hoogte = zone
+        if None in zone:
+            return False
+        return zone_x <= self.x <= zone_x + zone_breedte and zone_y <= self.y <= zone_y + zone_hoogte
+
+    def overdracht(self, zone1, overdracht1, zone2, overdracht2):
+        if self.in_zone(zone1):
+            self.x, self.y = overdracht1
+
+
+#-----------------------------------------------Speler_eten/botsing----------------------------------------------------#
+
+    def check_collision(self, spook):
+        speler_rect = pygame.Rect(self.x - self.straal + 3, self.y - self.straal + 3, (self.straal - 4) * 2,
+                                  (self.straal - 4) * 2)
+        return speler_rect.colliderect(spook.rect)
+
+    def eetspook(self, spook , tijd_van_bonus):
+        if self.eetmodus_actief(tijd_van_bonus) and self.check_collision(spook):
+            self.eetcombo += 1
+            punten = 50 * self.eetcombo
+            self.gegeten_spoken_teller += 1
+
+            # Check op extra leven
+            if self.gegeten_spoken_teller >= 10:
+                self.levens += 1
+                self.gegeten_spoken_teller = 0  # Reset teller
+
+            spook.reset()
+            return punten
+        return 0
+
+#-----------------------------------------------Speler_start-----------------------------------------------------------#
 
     def start_eetmodus(self):
         self.timer = time.time()  # actieve eetmodus met echte tijd
         self.eetcombo = 0  # reset combo wanneer nieuwe modus start
 
-    def eetmodus_actief(self):
+    def start_timer(self, duur_in_seconden):
+        self.timer = pygame.time.get_ticks()
+        self.timer_duur = duur_in_seconden * 1000  # omzetten naar milliseconden
+
+    def activeer_snelheid(self, boost, duur):
+        self.snelheid = self.snelheid_standaard + boost
+        self.snelheid_timer = pygame.time.get_ticks()
+        self.snelheid_duur = duur
+
+#-----------------------------------------------Speler_tools-----------------------------------------------------------#
+
+    def is_op_triggertegel(self, tegels):
+        tegel_x = self.x // muurgrootte
+        tegel_y = self.y // muurgrootte
+        return (tegel_x, tegel_y) in tegels
+
+    def eetmodus_actief(self, tijd_van_bonus):
         if self.timer is None:
             return False
         if time.time() - self.timer <= tijd_van_bonus:  # 30 seconden duren
@@ -191,52 +240,31 @@ class Speler:
             self.timer = None  # Zet terug naar None als tijd voorbij is
             return False
 
-    def botsing(self, spook):
-        spelerx1 = self.x - self.straal
-        spelerx2 = self.x + self.straal
-        spelery1 = self.y - self.straal
-        spelery2 = self.y + self.straal
-
-        x1 = spook.centerbalx - spook.straal + refactor
-        x2 = spook.centerbalx + spook.straal - refactor
-        y1 = spook.centerbaly - spook.straal + refactor
-        y2 = spook.centerbaly + spook.straal - refactor
-
-        return spelerx2 > x1 and spelerx1 < x2 and spelery2 > y1 and spelery1 < y2
-
-    def check_collision(self, spook):
-        speler_rect = pygame.Rect(self.x - self.straal + 3, self.y - self.straal + 3, (self.straal-4) * 2  , (self.straal-4) * 2 )
-        return speler_rect.colliderect(spook.rect)
-
-    def eetspook(self, spook):
-        if self.eetmodus_actief() and self.check_collision(spook):
-            self.eetcombo += 1
-            punten = 50 * self.eetcombo  # Combo: 50, 100, 150, ...
-            spook.reset()
-            return punten
-        return 0
-
-    def toon_timer(self, scherm, font , x_positie, y_positie , kleur):
-        if self.eetmodus_actief():
-            resterende_tijd = max(0, int(tijd_van_bonus - (time.time() - self.timer)))
-            tekst = font.render(f"Eetmodus: {resterende_tijd}s", True, kleur)  # Gele tekst
-            scherm.blit(tekst, (x_positie, y_positie))  # Positie linksboven
-
-    def start_timer(self, duur_in_seconden):
-        self.timer = pygame.time.get_ticks()
-        self.timer_duur = duur_in_seconden * 1000  # omzetten naar milliseconden
-
-    def is_op_triggertegel(self, tegels):
-        tegel_x = self.x // muurgrootte
-        tegel_y = self.y // muurgrootte
-        return (tegel_x, tegel_y) in tegels
-
-    def activeer_snelheid(self, boost=2, duur=50000):
-        self.snelheid += boost
-        self.snelheid_timer = pygame.time.get_ticks()
-        self.snelheid_duur = duur
-
     def update_snelheid(self):
         if self.snelheid_timer and pygame.time.get_ticks() - self.snelheid_timer > self.snelheid_duur:
             self.snelheid = self.snelheid_standaard
             self.snelheid_timer = None
+
+
+
+#---------------------------------------Toonfuncties-------------------------------------------------------------------#
+
+    def toon_timer(self, scherm, font , x_positie, y_positie , tijd_van_bonus , kleur):
+        if self.eetmodus_actief(tijd_van_bonus):
+            resterende_tijd = max(0, int(tijd_van_bonus - (time.time() - self.timer)))
+            tekst = font.render(f"Eetmodus: {resterende_tijd}s", True, kleur)  # Gele tekst
+            scherm.blit(tekst, (x_positie, y_positie))  # Positie linksboven
+
+    def toon_snelheidstimer(self, scherm, font, x_positie, y_positie, kleur):
+        if self.snelheid_timer:
+            verstreken = pygame.time.get_ticks() - self.snelheid_timer
+            resterende_ms = max(0, self.snelheid_duur - verstreken)
+            resterende_seconden = int(resterende_ms / 1000)
+            if resterende_seconden > 0:
+                tekst = font.render(f"Boost: {resterende_seconden}s", True, kleur)
+                scherm.blit(tekst, (x_positie, y_positie))
+
+    def toon_spook_teller(self, scherm, font, x, y, kleur):
+        tekst = font.render(f"Spoken: {self.gegeten_spoken_teller}/10", True, kleur)
+        scherm.blit(tekst, (x, y))
+
